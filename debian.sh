28bd443e6d13fc9ceef48354442636c4018a7b1c
Purnachand Medisetty <pmediset@akamai.com>
2023-04-27 22:05:07 +0530

    oiufds

--- debian.sh
+++ debian.sh
@@ -1,5 +1,5 @@
 apt update
-apt install iperf3 tree keepalived strongswan fping zsh wireguard-tools telnet frr most neofetch traceroute mtr nmap tcpdump sipcalc apache2 -y
+apt install iftop nload iperf3 tree keepalived strongswan fping zsh wireguard-tools telnet frr most neofetch traceroute mtr nmap tcpdump sipcalc apache2 -y
 
 
 
